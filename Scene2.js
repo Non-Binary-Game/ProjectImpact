e79630aec850ab4e88c6f3eeb6df43d57406ee6f
Destinee A. Adams <adestinee37@gmail.com>
2024-04-20 21:26:30 -0400

    gravity

--- Scene2.js
+++ Scene2.js
@@ -4,31 +4,34 @@ var Scene2 = new Phaser.Class({
 
     initialize:
 
-    function Scene2 ()
-    {
-        Phaser.Scene.call(this, { key: 'Scene2' });
-    },
+        function Scene2() {
+            Phaser.Scene.call(this, {
+                key: 'Scene2'
+            });
+        },
 
-    preload: function ()
-    {
+    preload: function () {
         // Load assets needed for scene2
-       
-        this.load.image('terrain', 'assets/terrain.png');
+
+        // this.load.image('terrain', 'assets/terrain.png');
         this.load.image('player', 'assets/player.png');
     },
 
-    create: function ()
-    {
+    create: function () {
         // Add any initialization code for scene2
 
-        player = this.physics.add.sprite(100, 450, 'player');
+        player = this.physics.add.sprite(100, 100, 'player');
         cursors = this.input.keyboard.createCursorKeys();
 
         // Create the terrain grid
         terrain = this.physics.add.group({
             key: 'terrain',
             repeat: 79,
-            setXY: { x: 0, y: 500, stepX: 10 }
+            setXY: {
+                x: 0,
+                y: 500,
+                stepX: 10
+            }
         });
 
         this.physics.world.setBounds(0, 0, this.cameras.main.width, this.cameras.main.height);
@@ -45,9 +48,30 @@ var Scene2 = new Phaser.Class({
         this.physics.add.collider(player, terrain, destroyCell, null, this);
     },
 
-    update: function ()
-    {
-        // Add any update logic for scene2
-        // Player movement, collision detection, etc.
+    update: function () {
+
+        // Player movement
+        if (cursors.left.isDown) {
+            player.setVelocityX(-160); // Move left
+        } 
+        
+        else if (cursors.right.isDown) {
+            player.setVelocityX(160); // Move right
+        } 
+        
+        else if (cursors.up.isDown) {
+            player.setVelocityY(-160); // Move up
+        }
+        
+        else if (cursors.down.isDown) {
+            player.setVelocityY(160); // Move down
+        }
+        
+        else {
+            player.setVelocityX(0); // Stop horizontal movement
+        }
+
+
+
     }
 });
