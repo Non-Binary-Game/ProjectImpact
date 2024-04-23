27962b32ee29ba7e61854b76c894a0e921abff02
Destinee A. Adams <adestinee37@gmail.com>
2024-04-22 23:06:39 -0400

    added player sprite and terrian

--- assets/globalAssets/globalAssetLoader.js
+++ assets/globalAssets/globalAssetLoader.js
@@ -3,7 +3,7 @@ function loadGlobalAssets(scene){
    //this.load.image('player', './globalAssets/player.png');
    
    //Load into Scene images
-   scene.load.image('player', 'assets/globalAssets/player.png');
+   scene.load.image('player', 'assets/spaeatepngfiles/player/walk/playerwalk1.png');
 
    //load into scene animations
 }
