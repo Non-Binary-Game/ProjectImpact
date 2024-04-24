8e3d5f46d18415806ce4eb49243f76a9cc1cbea4
RuebenHodges <hodgesr@umich.edu>
2024-04-24 18:32:31 -0400

    fixing animations

--- assets/globalAssets/globalAssetLoader.js
+++ assets/globalAssets/globalAssetLoader.js
@@ -3,7 +3,7 @@ function loadGlobalAssets(scene){
    //this.load.image('player', './globalAssets/player.png');
    
    //Load into Scene images
-   scene.load.image('player', 'assets/spaeatepngfiles/player/walk/playerwalk1.png');
+   //scene.load.image('player', 'assets/spaeatepngfiles/player/walk/playerwalk1.png');
    scene.load.spritesheet('player', 'assets/playerspriteSheet/playerwalksheet.png', { frameWidth: 32, frameHeight: 32 });
    //load into scene animations
 }
--- player/playerAnimations.js
+++ player/playerAnimations.js
@@ -5,6 +5,13 @@ const playerAnimations =(Scene) =>{
         frameRate: 10,
         repeat: -1
     });
+    /*
+    Scene.anims.create({
+        key: 'left',
+        frames: Scene.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });*/
 
 
 };
